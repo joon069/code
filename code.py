4ec5bd812619536ba04dcf8d3c11fc07282482f1
강준영 <j8521103@gmail.com>
2025-03-18 10:12:10 +0900

    // C

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
-a,b = map(float, input().split())
-print(format(a/b, ".3f"))
+a, b = map(float, input().split())
+
+if b == 0:
+    print("Error: Division by zero is not allowed.")
+else:
+    print(format(a/b, ".3f"))
 
 #코드업 43번 문제
 # 다른 애들은 다 알고리즘 푸는데 난 아직 풀지 못한다. 더 열심히 해야겠다.
