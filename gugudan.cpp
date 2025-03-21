97b717082b893f61b856aa92ddfc98b3f1877d55
강준영 <j8521103@gmail.com>
2025-03-21 21:35:01 +0900

    정사각형 만들기

--- gugudan.cpp
+++ gugudan.cpp
@@ -2,9 +2,11 @@
 
 int main(void)
 {
-    for(int i=1; i<=9; i++){
-        for(int j=1; j<=9; j++){
-            printf("%d ",i*j);
+    int a=0;
+    scanf("%d", &a);
+    for(int i=1; i<=a; i++){
+        for(int j=1; j<=a; j++){
+            printf("*");
         }
     printf("\n");
     }
