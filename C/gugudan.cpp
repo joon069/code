15c9fcf8b69cce5e87dcf9e1d229ba9f144f24f1
강준영 <j8521103@gmail.com>
2025-03-21 21:54:35 +0900

    삼각형 만들기 (별)

--- C/gugudan.cpp
+++ C/gugudan.cpp
@@ -4,7 +4,7 @@ int main(void)
 {
     for(int i=1; i<=9; i++){
         for(int j=1; j<=9; j++){
-            printf("%2d ",i*j);
+            printf("%2d",i*j);
         }
     printf("\n");
     }
