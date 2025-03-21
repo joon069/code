23081071e615604965d06beacd5c90e5de7c0b42
강준영 <j8521103@gmail.com>
2025-03-21 21:35:51 +0900

    사각형 만들기 (높이 너비 아니 맛있겠다.)

--- gugudan.cpp
+++ gugudan.cpp
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    int a=0;
-    scanf("%d", &a);
+    int a=0, b=0;
+    scanf("%d %d", &a, &b);
     for(int i=1; i<=a; i++){
-        for(int j=1; j<=a; j++){
+        for(int j=1; j<=b; j++){
             printf("*");
         }
     printf("\n");
