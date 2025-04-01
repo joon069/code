d5bb963dc16f98838c5283cd6d19a1e935abaedd
강준영 <j8521103@gmail.com>
2025-04-01 10:12:51 +0900

    아 코등 잘못 붙여 넣음 ㅎㅎ 내림 차순으로 정리

--- Python/AlgorithmA/0401Al.py
+++ Python/AlgorithmA/0401Al.py
@@ -3,9 +3,11 @@ def sel_sort(a):
     for i in range(0,n-1):
         min_idx = i
         for j in range(i+1, n):
-            if(a[j]<a[min_idx]):
+            if(a[j]>a[min_idx]):
                 min_idx = j
         a[i], a[min_idx] = a[min_idx], a[i]
 
-v = [17, 92, 18, 33, 58, 7, 33, 42]
-print(find_min(v))
+
+d = [2, 4, 5, 1, 3]
+sel_sort(d)
+print(d)
