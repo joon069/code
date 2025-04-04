1f93627947a3077965f3774cfbc8c2ef0ff6a794
강준영 <j8521103@gmail.com>
2025-04-04 11:48:08 +0900

    짝 지어주는 프로그램

--- Python/AlgorithmB/AlgoB404.py
+++ Python/AlgorithmB/AlgoB404.py
@@ -1,3 +1,4 @@
+#엘리스 1-7
 def find_same_name(a):
     n = len(a)
     result = set()
@@ -12,8 +13,8 @@ print(find_same_name(name))
 
 name2 = ["Tom", "Jerry", "Mike", "Tom", "Mike"]
 print(find_same_name(name2))
-
-#
+print()
+#엘리스 2-1
 def fact(n):
     result = 1
     for i in range(2, n+1):
@@ -23,3 +24,17 @@ def fact(n):
 print(fact(1))
 print(fact(5))
 print(fact(10))
+print()
+
+#엘리스 6-4
+def print_pairs(a):
+    n = len(a)
+    for i in range(0, n-1):
+        for j in range(i+1, n):
+            print(a[i], "-", a[j])
+
+name = ["Tom", "Jerry", "Mike"]
+print_pairs(name)
+print()
+name2 = ["Tom", "Jerry", "Mike", "John"]
+print_pairs(name2)
