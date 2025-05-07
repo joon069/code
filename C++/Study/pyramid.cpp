07a2514d842b0eaf3f5e73d47c7deb16795b536b
강준영 <j8521103@gmail.com>
2025-05-07 11:01:32 +0900

    피라미드

--- C++/Study/pyramid.cpp
+++ C++/Study/pyramid.cpp
@@ -3,14 +3,17 @@
 //
 #include <stdio.h>
 int main(void){
-    int n;
+    int n,i,j,k;
     printf("몇 단 삼각형입니까?: ");
     scanf("%d", &n);
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++) {
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n-i; j++)
+        {
             printf(" ");
         }
-        for (int k = 0; k < (i-1)*2+1; k++) {
+        for (k = 0; k < i * 2 + 1; k++)
+        {
             printf("*");
         }
         printf("\n");
