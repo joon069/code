e8071dfac63e47361b5e2dc4f85276b2e56c7f2f
강준영 <j8521103@gmail.com>
2025-05-07 12:13:42 +0900

    피라미드 홀수만 먹음

--- C++/Study/pyramid.cpp
+++ C++/Study/pyramid.cpp
@@ -6,13 +6,13 @@ int main(void){
     int n,i,j,k;
     printf("몇 단 삼각형입니까?: ");
     scanf("%d", &n);
-    for (i = 0; i < n; i++)
+    for (i = 1; i <= n; i+=2)
     {
-        for (j = 0; j < n-i; j++)
+        for (j = 0; j < (n-i)/2; j++)
         {
             printf(" ");
         }
-        for (k = 0; k < i * 2 + 1; k++)
+        for (k = 0; k < i; k++)
         {
             printf("*");
         }
