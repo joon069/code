949b215e38c7273a5d9db635f61851e684069709
강준영 <j8521103@gmail.com>
2025-05-08 12:01:34 +0900

    알고리즘 B 딕셔너리로 학생 번호에 해당하는 학생 이름 찾기

--- Python/AlgorithmB/AlgoB508.py
+++ Python/AlgorithmB/AlgoB508.py
@@ -18,3 +18,19 @@ def palindrome(a):
 print(palindrome("Wow"))
 print(palindrome("Madam, I’m Adam."))
 print(palindrome("Madam, I am Adam."))
+
+#---------------------------------------------------
+
+def get_name(s_info, find_no):
+    if find_no in s_info:
+        return s_info[find_no]
+    else: return "?"
+sample_info = {
+    39: "Justin",
+    14: "John",
+    67: "Mike",
+    105: "Summer"
+}
+
+print(get_name(sample_info, 105))
+print(get_name(sample_info, 777))
