4bd42ca7f71d800a44409aa65b6e22140d97f363
강준영 <j8521103@gmail.com>
2025-05-21 17:34:59 +0900

    배열에 7개의 숫자를 입력 받고, 거꾸로 출력하는 프로그램.

---
+++ C++/ProgrammingA/521class/array4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+  int seven[7];
+    for(int i=0; i<7; i++){
+        scanf("%d", &seven[i]);
+    }
+    for(int i=6; i>=0; i--){
+        printf("%d ", seven[i]);
+    }
+    return 0;
+}
