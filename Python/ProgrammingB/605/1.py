5f64ecf7f041a7448b15ee8f73786a8351af96ca
강준영 <j8521103@gmail.com>
2025-06-10 00:09:08 +0900

    프로그래밍 B 과제

---
+++ Python/ProgrammingB/605/1.py
@@ -0,0 +1,66 @@
+#40page
+
+# friend=[]
+# name=input('친구 이름 입력: ')
+# while name != '':
+#     friend.append(name)
+#     name=input('친구 이름 입력: ')
+# print(friend)
+
+# menu = []
+# menu.append('치킨')
+# menu.append('피자')
+# menu.append('샐러드')
+# menu.append('스테이크')
+# print(menu)
+
+# menu.remove('스테이크')
+# menu.append('감자튀김')
+# print(menu)
+
+# menu.remove('샐러드')
+# print(menu)
+# print("샐러드 메뉴가 삭제 되었습니다.")
+
+# import random
+# number = [10, 20, 33, 40, 55 ,60, 77, 80, 90, 100]
+# a=random.randint(0,9)
+# b=random.randint(0,9)
+# ans = int(input(f"{number[a]} + {number[b]} = ?"))
+# if ans == number[a] + number[b]:
+#     print("맞았습니다.")
+# else:
+#     print("틀렸습니다.")
+
+# a = [7,6,5,4]
+# b = [1,2,3]
+# print(b + a)
+# print(a + b*2)
+# print(b[0:2]+a[0:2]*2)
+
+# a = [1,2,3,4,5]
+# if 1 in a:
+#   print("1이 존재합니다")
+# else:
+#   print("1이 존재하지 않습니다")
+
+# a = [1,2,3,4,5]
+# if 0 in a:
+#   print("0이 존재합니다")
+# else:
+#   print("0이 존재하지 않습니다")
+
+# import random
+# alist = []
+# num = random.randint(1,100)
+# while len(alist) < 5:
+#     alist.append(num)
+#     num = random.randint(1,100)
+# print(alist)
+# if 50 in alist:
+#     print("50이 존재합니다.") 
+#     print(alist)
+# else:
+#     print("50이 존재하지 않습니다.")
+#     print(alist)
+
