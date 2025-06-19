8851945a50c232b27060889bb219578945d01f76
강준영 <j8521103@gmail.com>
2025-06-19 16:08:02 +0900

    프르그래밍B 수업 내용

---
+++ Python/ProgrammingB/11.py
@@ -0,0 +1,45 @@
+# def sum(a,b):
+#   return a+b
+# print(sum(5,6))
+
+# def avg(m, s, i):
+#   averge = (m + s + i)/ 3
+#   return averge
+# mat = int(input("수학 "))
+# sic = int(input("과학 "))
+# inf = int(input("정보 "))
+# result = avg(mat, sic, inf)
+# print(result)
+
+# def greatest(data):
+#   greater = data[0]
+#   for i in range(1, len(data)):
+#     if greater < data[i]:
+#       greater = data[i]
+#   return greater
+
+# nums = [75, 80, 50, 100, 95, 90, 65, 80, 70]
+# result = greatest(nums)
+# print(result)
+
+# def plus():
+#   s = s + 1
+
+#에러 뜨는 예시
+# s = 0
+# plus()
+# plus()
+# print(s)
+
+
+# from random import *
+# from math import *
+
+# def s(a,b):
+#   j = a if a > b else b
+#   print(j)
+
+# s(4,5)
+
+
+
