71e9c284ab5d0e48f4ae8d62803cf04288b340cb
강준영 <j8521103@gmail.com>
2025-07-01 17:43:53 +0900

    출력 형식 복습

--- C++/Study/701/엄.cpp
+++ C++/Study/701/엄.cpp
@@ -1,27 +1,8 @@
 #include <stdio.h>
-int AND(int a, int b);
-int OR(int a, int b);
-int NOT(int n);
-
-int main()
+int main(void)
 {
-  int a, b;
-  scanf("%d %d\n", &a, &b);
-
-  printf("%d\n", AND(a, b));
-  printf("%d\n", OR(a, b));
-  printf("%d\n", NOT(a));
-  printf("%d\n", NOT(b));
-  
-  return 0;
-}
-
-int AND(int a, int b){
-  return a && b;
-}
-int OR(int a, int b){
-  return a || b;
-}
-int NOT(int n){
-  return !n;
+  double a;
+  double *p = &a;
+  scanf("%lf", p);
+  printf("%.2lf", *p / 2);
 }
