6b27782ae5d110ee2967aa6e6be333be07ef7304
강준영 <j8521103@gmail.com>
2025-07-01 17:53:53 +0900

    반복문 do while 문 복습

--- C++/Study/701/엄.cpp
+++ C++/Study/701/엄.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-  double a;
-  double *p = &a;
-  scanf("%lf", p);
-  printf("%.2lf", *p / 2);
+  int n = 0;
+  do
+  {
+    n++;
+    printf("%d\n", n);
+  } while (n != 10);
+  printf("어 끝\n");
+  return 0;
 }
